db73ac81553ed37b28b9b1ce369c94fbaaa03fb6
Sanath K S <sanathselvakumaran@gmail.com>
2026-01-05 14:16:47 +0000

    The content inside the modal box must be black which right now is grey.

--- src/components/landing/DownloadWorkbookModal.tsx
+++ src/components/landing/DownloadWorkbookModal.tsx
@@ -20,8 +20,8 @@ export function DownloadWorkbookModal({ children }: { children: React.ReactNode 
         <DialogHeader className="text-center">
           <DialogTitle className="text-2xl font-headline">One more thing... ✨</DialogTitle>
           <DialogDescription asChild>
-            <div className="text-base text-foreground pt-2">
-            I am charging a small amount of ₹49 for this workbook because creating it took a lot of time and energy. I am very confident that this workbook will help you become aware of your real passion and talent. It is truly worth it.
+            <div className="text-lg text-foreground pt-2">
+            <p>I am charging a small amount of ₹49 for this workbook because creating it took a lot of time and energy. I am very confident that this workbook will help you become aware of your real passion and talent. It is truly worth it.</p>
             <p className="text-red-500 mt-2">Kindly take a printout of the PDF after you download.</p>
             </div>
           </DialogDescription>
